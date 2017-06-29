7419f11f31801dc3f1f0ffbb01f5d9ab9367897f
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2017-06-29 23:28:32 +0200

    Update the justification services due to the changes in the interface

--- src/main/java/org/liveontologies/protege/justification/blackbox/BlackBoxJustificationComputationManager.java
+++ src/main/java/org/liveontologies/protege/justification/blackbox/BlackBoxJustificationComputationManager.java
@@ -1,41 +1,11 @@
 package org.liveontologies.protege.justification.blackbox;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-
-/*-
- * #%L
- * Protege Blackbox Justification Explanation
- * $Id:$
- * $HeadURL:$
- * %%
- * Copyright (C) 2016 - 2017 Live Ontologies Project
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.AbstractAction;
-import javax.swing.ButtonGroup;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 
 import org.liveontologies.protege.explanation.justification.service.JustificationComputation;
 import org.liveontologies.protege.explanation.justification.service.JustificationComputation.InterruptMonitor;
@@ -70,7 +40,7 @@ public class BlackBoxJustificationComputationManager
 		computation_ = new BlackBoxJustificationComputation(
 				getJustificationListener(), getInterruptMonitor(),
 				getEntailment(), kit_, workbenchSettings_);
-		notifyComputationChanged();
+		notifyJustificationsOutdated();
 	}
 
 	@Override
